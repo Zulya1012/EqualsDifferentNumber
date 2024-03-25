021fcbd554e0d3008105c1216d9e45f3c7681e5e
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-03-25 12:35:46 +0400

    Update differentNumber.java

--- differentNumber.java
+++ differentNumber.java
@@ -1,28 +1,22 @@
 import java.util.Scanner;
 public class differentNumber {
 
-  public static void main(String[] args)
-    {
-        Scanner s = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
 
-        System.out.println("Input first number: ");
-        int x = s.nextInt();
-        System.out.println("Input second number: ");
-        int y = s.nextInt();
-        System.out.println("Input third number: ");
-        int z = s.nextInt();
+    System.out.println("Input first number: ");
+    int x = s.nextInt();
+    System.out.println("Input second number: ");
+    int y = s.nextInt();
+    System.out.println("Input third number: ");
+    int z = s.nextInt();
 
-        if (x == y && x == z)
-        {
-            System.out.println("All numbers are equal");
-        }
-        else if ((x == y) || (x == z) || (z == y))
-        {
-            System.out.println("Neither all are equal or different");
-        }
-        else
-        {
-            System.out.println("All numbers are different");
-        }
+    if (x == y && x == z) {
+      System.out.println("All numbers are equal");
+    } else if ((x == y) || (x == z) || (z == y)) {
+      System.out.println("Neither all are equal or different");
+    } else {
+      System.out.println("All numbers are different");
     }
+  }
 }
